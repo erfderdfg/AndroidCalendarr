837603b9f19a1c4f1402aed514b6f81c60174336
Mollyyy7 <101139130+Mollyyy7@users.noreply.github.com>
2023-12-30 11:12:11 -0500

    basic calendar revision

--- app/src/main/java/com/example/androidcalendarr/MainActivity.java
+++ app/src/main/java/com/example/androidcalendarr/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.androidcalendarr;
 
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    CalendarView simpleCalendarView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        simpleCalendarView = (CalendarView) findViewById(R.id.simpleCalendarView); // get the reference of CalendarView
+        // perform setOnDateChangeListener event on CalendarView
+        simpleCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                // display the selected date by using a toast
+                Toast.makeText(getApplicationContext(), dayOfMonth + "/" + month + "/" + year, Toast.LENGTH_LONG).show();
+            }
+        });
     }
+
+
 }
